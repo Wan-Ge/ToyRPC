04729dc664ca547f6d6fa3750c857708a1f71669
Wan-Ge <ynxyng@gmail.com>
2018-08-28 00:13:01 +0800

    add handler

--- toyRPC/src/main/java/com/zxcvs/server/RpcHandler.java
+++ toyRPC/src/main/java/com/zxcvs/server/RpcHandler.java
@@ -16,7 +16,21 @@
  */
 package com.zxcvs.server;
 
+import com.alibaba.fastjson.JSON;
+import com.zxcvs.protocol.RpcRequest;
+import com.zxcvs.protocol.RpcResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.experimental.var;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import net.sf.cglib.reflect.FastClass;
+import net.sf.cglib.reflect.FastMethod;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 /**
  * RPC handler - RPC request processor
@@ -25,5 +39,56 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * Create at 2018/8/27 13:48
  */
 
-public class RpcHandler extends SimpleChannelInboundHandler<> {
+@Slf4j
+public class RpcHandler extends SimpleChannelInboundHandler<RpcRequest> {
+
+    private final Map<String, Object> handlerMap;
+
+    public RpcHandler(Map<String, Object> handlerMap) {
+        this.handlerMap = handlerMap;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, RpcRequest rpcRequest) {
+        RpcServer.submit(() -> {
+            log.debug("Receive request: " + rpcRequest.getRequestId());
+            RpcResponse rpcResponse = new RpcResponse();
+            rpcResponse.setRequestId(rpcRequest.getRequestId());
+            try {
+                Object result = handle(rpcRequest);
+                rpcResponse.setResult(result);
+            } catch (InvocationTargetException e) {
+                rpcResponse.setError(e.toString());
+                log.error("RPC server handle request error: {}", e);
+            }
+
+            channelHandlerContext.writeAndFlush(rpcResponse).addListener((ChannelFutureListener) future ->
+                    log.debug("Send response for request: {}", rpcRequest.getRequestId()));
+        });
+    }
+
+    private Object handle(RpcRequest request) throws InvocationTargetException {
+        String className = request.getClassName();
+        Object serviceBean = handlerMap.get(className);
+
+        // get class and other info
+        Class<?> serviceClass = serviceBean.getClass();
+        String methodName = request.getMethodName();
+        Class<?>[] paramTypes = request.getParamTypes();
+        Object[] params = request.getParams();
+
+        log.debug("ClassName: {}, methodName: {}", serviceClass.getName(), methodName);
+        log.debug("params: {}, {}", JSON.toJSONString(paramTypes), JSON.toJSONString(params));
+
+        // cglib reflect
+        final FastClass serviceFastClass = FastClass.create(serviceClass);
+        final FastMethod method = serviceFastClass.getMethod(methodName, paramTypes);
+        return method.invoke(serviceBean, params);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error("server caught exception: {}", cause);
+        ctx.close();
+    }
 }
