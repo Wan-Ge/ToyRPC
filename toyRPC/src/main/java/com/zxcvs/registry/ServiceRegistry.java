969b61c350edf9d04dc448e8b64dae717b959c27
Wan-Ge <ynxyng@gmail.com>
2018-08-27 15:06:55 +0800

    ZooKeeper dependency and registry

---
+++ toyRPC/src/main/java/com/zxcvs/registry/ServiceRegistry.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zxcvs.registry;
+
+import com.sun.istack.internal.NotNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Service registry
+ *
+ * @author Xiaohui Yang
+ * Create at 2018/8/27 14:17
+ */
+
+@Slf4j
+public class ServiceRegistry {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @NotNull private String registryAddress;
+
+    public ServiceRegistry(String registryAddress) {
+        this.registryAddress = registryAddress;
+    }
+
+    /**
+     * registry to zookeeper
+     *
+     * @param data  the data of node
+     */
+    public void registry(String data) {
+        if (StringUtils.isEmpty(data)) {
+            ZooKeeper zk = connectServer();
+            if (zk != null) {
+                // Add root node if not exist
+                addRootNode(zk);
+                createNode(zk, data);
+            }
+        }
+    }
+
+    /**
+     * connect to server
+     *
+     * @return zookeeper instance
+     */
+    private ZooKeeper connectServer() {
+        ZooKeeper zk = null;
+        try {
+            zk = new ZooKeeper(registryAddress, Constant.ZK_SESSION_TIMEOUT, watchedEvent -> {
+                if (watchedEvent.getState() == Watcher.Event.KeeperState.SyncConnected) {
+                    latch.countDown();
+                }
+            });
+            latch.await();
+        } catch (IOException | InterruptedException e) {
+            log.error("connect server error: ", e);
+        }
+        return zk;
+    }
+
+    /**
+     * add root node if not exist
+     *
+     * @param zk    current zookeeper
+     */
+    private void addRootNode(ZooKeeper zk) {
+        try {
+            // TODO: exists() add comments
+            Stat stat = zk.exists(Constant.ZK_REGISTRY_PATH, false);
+            if (stat == null) {
+                zk.create(Constant.ZK_REGISTRY_PATH, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (KeeperException | InterruptedException e) {
+            log.error("add root node error: ", e);
+        }
+    }
+
+    /**
+     * add node below root
+     *
+     * @param zk    current zookeeper
+     * @param data  the data of node
+     */
+    private void createNode(ZooKeeper zk, String data) {
+        try {
+            byte[] bytes = data.getBytes();
+            String path = zk.create(Constant.ZK_DATA_PATH, bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            log.debug("create zookeeper node ({} => {})", path, data);
+        } catch (KeeperException | InterruptedException e) {
+            log.error("add root node error: ", e);
+        }
+    }
+
+}
