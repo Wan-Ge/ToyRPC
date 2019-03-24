62f37f647fe4c0d14fc32d70c0f6c20de10d3214
Wan-Ge <ynxyng@gmail.com>
2019-03-24 17:44:42 +0800

    增加测试启动类

---
+++ src/test/java/com/zxcvs/test/RpcBootStrap.java
@@ -0,0 +1,16 @@
+package com.zxcvs.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xiaohui Yang
+ * Create at 2019/3/24 16:23
+ */
+
+@SpringBootApplication(scanBasePackages = "com.zxcvs.*")
+public class RpcBootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(RpcBootStrap.class);
+    }
+}
