75394bfe991abcf706584fd824e6419197385ec6
Wan-Ge <ynxyng@gmail.com>
2019-03-25 20:27:18 +0800

    修改扫包和配置注解

--- src/test/java/com/zxcvs/test/RpcBootStrap.java
+++ src/test/java/com/zxcvs/test/RpcBootStrap.java
@@ -1,14 +1,18 @@
 package com.zxcvs.test;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Xiaohui Yang
  * Create at 2019/3/24 16:23
  */
 
-@SpringBootApplication(scanBasePackages = "com.zxcvs.*")
+@Component
+@ComponentScan(basePackages = "com.zxcvs.*")
+@PropertySource({"classpath:rpc.properties"})
 public class RpcBootStrap {
     public static void main(String[] args) {
         SpringApplication.run(RpcBootStrap.class);
